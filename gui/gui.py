e2e6d042fdc5d52616b86b26c174bd10b2894612
avariiinaa <log7548@gmail.com>
2024-03-15 13:39:11 +0300

    gui added

---
+++ gui/gui.py
@@ -0,0 +1,43 @@
+from tkinter import Button, Label, Tk
+from tkvideo import tkvideo
+from sys import argv
+
+
+def start_demo():
+    print("start demonstration")
+
+
+def end_demo():
+    print("end demonstration")
+
+
+def emergency():
+    print("emergency stop")
+
+
+window = Tk()
+window.title("aruco tracker")
+
+# buttons init
+button_start = Button(window, text="Начать демонстрацию", width=20, height=5, bg='white', fg='black',
+                      command=start_demo)
+button_end = Button(window, text="Закончить демонстрацию", width=20, height=5, bg='white', fg='black', command=end_demo)
+button_emergency = Button(window, text="Аварийная остановка", width=20, height=5, bg='white', fg='black',
+                          command=emergency)
+
+# video init
+my_label = Label(window)
+my_label.pack()
+
+if len(argv)==1:
+    video_name = "video.mp4"
+else:
+    video_name = f"""{argv[1]}"""
+player = tkvideo(video_name, my_label, loop=1, size=(600, 480))
+player.play()
+
+button_start.pack()
+button_end.pack()
+button_emergency.pack()
+
+window.mainloop()
