25e53121a221aedfd341c7280d8d131dd5d28b2a
Danila Simanok <simanokda.etu@gmail.com>
2024-04-05 21:58:45 +0300

    marker created

--- minimal-example/setup.py
+++ minimal-example/setup.py
@@ -5,7 +5,10 @@ package_name = 'aurco_follower'
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
-data_files.append(('share/' + package_name + '/worlds', ['worlds/mavic_playground.wbt']))
+data_files.append(('share/' + package_name + '/worlds', [
+    'worlds/mavic_playground.wbt',
+    'worlds/aruco-marker-ID=228.png'
+]))
 data_files.append(('share/' + package_name + '/resource', ['resource/aurco_follower.urdf']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
