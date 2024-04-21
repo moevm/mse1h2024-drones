f36bb0480c8d937fb360279f963b0d5eb66f7786
Cottoffey <greatktoshka@gmail.com>
2024-04-21 23:52:03 +0300

    Fixed publishers

--- ARuco_Detect/Node_detect_ArUco.py
+++ ARuco_Detect/Node_detect_ArUco.py
@@ -1,8 +1,10 @@
+
 import cv2
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import Int32MultiArray
 import cv2.aruco as aruco
 import numpy as np
 
@@ -21,7 +23,7 @@ class ArucoDetector(Node):
             10) #take image from topic /Image
         self.publisher = self.create_publisher(Image, '/marked', 10)  # Send processed image to /marked
         self.bridge = CvBridge()
-        self.marker_publisher = self.create_publisher(Marker, '/marker', 10) # Send coordinates to /marker
+        self.marker_publisher = self.create_publisher(Int32MultiArray, '/marker', 10) # Send coordinates to /marker
 
     def image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
@@ -32,6 +34,7 @@ class ArucoDetector(Node):
     def detect_aruco_markers(self, image):
         # Camera dimensions and calibration parameters (default values)
         # need to change specifications by camera drone
+
         focal_length_x = 640
         focal_length_y = 640
         principal_point_x = 320
@@ -64,15 +67,16 @@ class ArucoDetector(Node):
                     marker_position = tvecs[i][0]
 
                     # Create and publish marker message
-                    marker_msg = Marker(marker_id=ids[i], position=marker_position)
-                    self.marker_publisher.publish(marker_msg)
+                    msg = Int32MultiArray()
+                    msg.data = marker_position
+                    self.marker_publisher.publish(msg)
 
                     # Draw marker ID and coordinate axes
                     cv2.putText(image, f"ID: {ids[i]}", (int(corners[i][0][0][0]), int(corners[i][0][0][1])),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                     cv2.drawAxis(marked_image, cam_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.1)
-
-        return marked_image
+                return marked_image
+        return image
 
 def main(args=None):
     rclpy.init(args=args)
