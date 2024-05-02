ae8bc6f1ef14ec1d4c9a7f62a842b514b00bd8ea
Cottoffey <greatktoshka@gmail.com>
2024-05-02 19:13:01 +0300

    Added colcon test

--- aruco_follower_ws/src/aruco_follower/test/test_pid.py
+++ aruco_follower_ws/src/aruco_follower/test/test_pid.py
@@ -1,24 +1,24 @@
 import pytest 
 
-from ..aurco_follower.pid_controller import PIDController
+from aruco_follower.pid_controller import PIDController
 from math import isclose
 
-def test_simple_case(self):
+def test_simple_case():
     pctr = PIDController()
     move = pctr.calculate(0, 1, 1)
     assert (isclose(move, 1.51, rel_tol=1e-7))
 
-def test_same_point(self):
+def test_same_point():
     pctr = PIDController()
     move = pctr.calculate(0, 0, 1)
     assert move == 0
 
-def test_negative_numbers(self):
+def test_negative_numbers():
     pctr = PIDController()
     move = pctr.calculate(-1, -10, 1) 
     assert (isclose(move, -13.59, rel_tol=1e-7))
 
-def test_two_steps(self):
+def test_two_steps():
     pctr = PIDController()
     x0 = 0
     x = 10
@@ -26,7 +26,7 @@ def test_two_steps(self):
     move = pctr.calculate(x0, x, 1)
     assert (isclose(move, -2.800999999999999, rel_tol=1e-7))
 
-def test_dt_zero(self):
+def test_dt_zero():
     pctr = PIDController()
     move = pctr.calculate(0, 1, 0)
     assert move == None
